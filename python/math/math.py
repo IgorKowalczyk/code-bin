e4bf4a808401ea981e87fd05bebce4203bf6324c
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-09 13:27:38 +0200

    Update math.py

--- python/math/math.py
+++ python/math/math.py
@@ -1,5 +1,7 @@
 a = int(input("Please enter first number: "));
 b = int(input("Please enter second number: "));
 obwod = (2*a*b);
+# Obwód 
 field = (a*b);
+# Pole
 print("The circuit has:", obwod, "(x)\nThe field is:", field, "(x)");
