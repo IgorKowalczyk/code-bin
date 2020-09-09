64dbf77fe26a47a436b7620a390871daeb4e03d3
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-09 13:23:23 +0200

    Create math.py

---
+++ python/math/math.py
@@ -0,0 +1,5 @@
+a = int(input("Please enter first number: "));
+b = int(input("Please enter second number: "));
+obwod = (2*a*b);
+field = (a*b);
+print("The circuit has:", obwod, "(x)\nThe field is:", field, "(x)");
