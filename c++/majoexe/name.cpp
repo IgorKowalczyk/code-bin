f702175db234e5e551ecc3175d93b40f51a0d0da
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-12 12:49:51 +0200

    Update and rename majo.cpp to name.cpp

---
+++ c++/majoexe/name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h> 
+using namespace std;
+
+int hello() {
+ string name;
+ cout << "\n\nPlease enter your name: ";
+ cin >> name;
+ cout << "\nYour name is " << name << ".";
+}
+
+int main() {
+ cout << "My first C++ Program!";
+ hello();
+}
