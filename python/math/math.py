f67301b689cd36416725bfc6f2eb53a7d36092d1
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-23 18:38:49 +0200

    Update math.py

--- python/math/math.py
+++ python/math/math.py
@@ -1,7 +1,19 @@
+# ---
+# File name: math.py
+# Note: This file may contain errors and bugs. Do not use it on production
+# Creator: Igor Kowalczyk
+# ---
+import time
+print("  __  __       _   _                   ");
+print(" |  \/  | __ _| |_| |__    _ __  _   _ ");
+print(" | |\/| |/ _` | __| '_ \  | '_ \| | | |");
+print(" | |  | | (_| | |_| | | |_| |_) | |_| |");
+print(" |_|  |_|\__,_|\__|_| |_(_) .__/ \__, |");
+print("                          |_|    |___/ \n\n");
+
 a = int(input("Please enter first number: "));
 b = int(input("Please enter second number: "));
-obwod = (2*a*b);
-# Obwód 
+circuit = (2*a*b);
 field = (a*b);
-# Pole
-print("The circuit has:", obwod, "(x)\nThe field is:", field, "(x)");
+print("\nThe circuit has: " + str(circuit) + "x \nThe field is: " + str(field) + "x");
+time.sleep(60);
