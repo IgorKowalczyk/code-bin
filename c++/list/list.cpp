a0aa6930cc44acf81a263301536fe514e109be7d
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-29 12:11:40 +0200

    Create list.cpp

---
+++ c++/list/list.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ float a;
+ cout << "Podaj pierwsza liczbe: ";
+ cin >> a;
+ for(int i = 0; i<=a ; i++) 
+  cout << "Liczba: " << i << "\n";
+}
