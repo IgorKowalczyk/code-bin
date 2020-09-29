da0b2b80cdd1d8382eb32e8b0dbec4fbaaddc4e6
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-29 12:14:59 +0200

    Update list.cpp

--- c++/list/list.cpp
+++ c++/list/list.cpp
@@ -1,8 +1,14 @@
+// ---
+// File name: list.cpp
+// Note: This file may contain errors and bugs. Do not use it on production
+// Creator: Igor Kowalczyk
+// ---
+ 
 #include <iostream>
 using namespace std;
 
 int main() {
- float a;
+ int a;
  cout << "Podaj pierwsza liczbe: ";
  cin >> a;
  for(int i = 0; i<=a ; i++) 
