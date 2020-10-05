4f2324ed76f361360775784983b7a1361195ea3a
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-05 14:53:22 +0200

    Create pizza.php

---
+++ php/pizza.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Database `majodb`
+ */
+
+/* `majodb`.`klient` */
+$klient = array(
+  array('ID Klienta' => '1','Imie' => 'Jan','Nazwisko' => 'Nowak','Ulica' => 'Główna','Numer' => '20'),
+  array('ID Klienta' => '2','Imie' => 'Marcin','Nazwisko' => 'Kowalski','Ulica' => 'Szkolna','Numer' => '40')
+);
+
+/* `majodb`.`pizza` */
+$pizza = array(
+  array('ID Klienta' => '1','Pizza' => 'Peperoni','Rozmiar' => 'Xl','Cena' => '20'),
+  array('ID Klienta' => '2','Pizza' => 'Margarita','Rozmiar' => 'L','Cena' => '15')
+);
+
+/* `majodb`.`zamówienia` */
+$zamówienia = array(
+);
