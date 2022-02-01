001186eba05e87fc5b8b02b136203252e7d525bd
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-02-01 11:53:16 +0100

    Create hello.asm

---
+++ assembly/hello.asm
@@ -0,0 +1,15 @@
+section .text
+global _start
+_start:
+ mov edx,len
+ mov ecx,msg
+ mov ebx,1
+ mov eax,4
+ int 0x80
+ mov ebx,0
+ mov eax,1 
+ int 0x80
+
+section .data
+msg db "Hello, world! ~Igor Kowalczyk",0xa
+len equ $ - msg
