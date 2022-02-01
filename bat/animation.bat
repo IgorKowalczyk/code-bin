b579cde94279e63564006e527f2205be99d36d30
Igor Kowalczyk <xxmajonezxx@gmail.com>
2022-02-01 11:55:21 +0100

    Create animation.bat

---
+++ bat/animation.bat
@@ -0,0 +1,85 @@
+@echo off
+color 0a
+title MARIHUANEN
+:0
+echo                                          +
+timeout 0 > nul
+echo                                          +
+timeout 0 > nul
+echo                                         ++
+timeout 0 > nul
+echo                                        +**-
+timeout 0 > nul
+echo                                       :++++-
+timeout 0 > nul
+echo            -:                        ::+++++
+timeout 0 > nul
+echo             :*-                      *******-
+
+echo             -++-                    :+++++++-
+timeout 0 > nul
+echo             -++++:-                 :+++++++-                     : -
+timeout 0 > nul
+echo              :**==*=:              ++********-                 =**
+timeout 0 > nul
+echo              -:+++++++-             :++++++++-              :++++:
+timeout 0 > nul
+echo               :+++++++++            :++++++++-           :+++++++
+timeout 0 > nul
+echo                :=********=-        +******=**         :*********
+timeout 0 > nul
+echo                 -++++++++++-       ++++++++++      --+++++++++:
+timeout 0 > nul
+echo                   :++++++++++      :++++++++:     :+++++++++
+timeout 0 > nul
+echo                   :****=***=*=-     +****=*=*-   ***********
+timeout 0 > nul
+echo                    -++++++++++:     :++++++:+  -++++++++:::
+timeout 0 > nul
+echo    -:-               :+++++++++:-  :++++++++- +++++++++++
+timeout 0 > nul
+echo      :*******:        +**********-  +******************
+timeout 0 > nul
+echo       -:++++++++++:-    --+++++:::: ::+++++::+++++:+::
+timeout 0 > nul
+echo          :+++++++++++*++:::+++*****:-+*****:+*****+*              :-+: :
+timeout 0 > nul
+echo            :***************:*******+-:*=+*+++****=        +******************+
+timeout 0 > nul
+echo              --::+++::+++::::+:+:+::+::++-:++++-: ----+::::+:+:::++++::::--
+timeout 0 > nul
+echo                    :*+**+*****+**+:+*:*+*+++: -++***************++:
+timeout 0 > nul
+echo                      :+********+****+*++*+++ ++***+**********+++
+timeout 0 > nul
+echo                       -::::::+++++:+++++++:+++++:+--:--- --
+timeout 0 > nul
+echo                    :**************+****+**+***********+:
+timeout 0 > nul
+echo                  :+++++*+++++++++++:++*+++++**+++++++++*++*+
+timeout 0 > nul
+echo             -::++++++++:----- --::+++-:++ -+++++++::++++++++++:-
+timeout 0 > nul
+echo           -+:                +******- :*++   +****+  +***********+
+timeout 0 > nul
+echo                            ::*++++     ::+     ++++        ++++++++
+timeout 0 > nul
+echo                            -:--          :       --+                - 
+timeout 0 > nul
+echo                                          +:
+timeout 0 > nul
+echo                                          :+
+timeout 0 > nul
+echo                                           :-
+timeout 0 > nul
+echo                                           :                                                                                                                                  
+timeout 0 > nul
+echo                                          -
+timeout 0 > nul
+echo                                         ---                                                                                                                                  
+timeout 0 > nul
+echo                                  *** MARIHUANEN ***                                         
+timeout 0 > nul
+echo                                         ---
+timeout 0 > nul
+echo                                          -           
